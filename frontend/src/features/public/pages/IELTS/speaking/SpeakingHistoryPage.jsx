f3fab3685d4a4805232facabfb308e17e172960c
Luan_Vo <thanhluanvo25@gmail.com>
2026-03-31 21:13:48 +0700

    Change import useSpeakingHisroty

--- frontend/src/features/public/pages/IELTS/speaking/SpeakingHistoryPage.jsx
+++ frontend/src/features/public/pages/IELTS/speaking/SpeakingHistoryPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSpeakingHistory } from '../../hooks/speaking/useSpeakingHistory';
+import { useSpeakingHistory } from '../../../hooks/IELTS/speaking/useSpeakingHistory';
 import {
   ArrowLeft,
   Calendar,
