211bdc540c7cb3009fb64605694ac4f799cda334
Luan_Vo <thanhluanvo25@gmail.com>
2026-03-31 21:23:37 +0700

    Change import api in useWrtingList

--- frontend/src/features/public/hooks/IELTS/writing/useWritingList.js
+++ frontend/src/features/public/hooks/IELTS/writing/useWritingList.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useMemo } from 'react';
-import { writingStudentApi } from '../../api/writingStudentApi';
+import { writingStudentApi } from '../../../api/IELTS/writing/writingStudentApi';
 
 export const useWritingList = () => {
     // Basic states for tests and user interactions
