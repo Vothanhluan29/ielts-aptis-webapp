85f4d01b3c3630d1cddf60aed552bd203424def1
Luan_Vo <thanhluanvo25@gmail.com>
2026-04-05 14:57:01 +0700

    Release old files

--- frontend/src/features/admin/components/listening/question-types/index.jsx
+++
@@ -1,5 +0,0 @@
-// Export với tên mới rõ ràng hơn
-export { default as ListeningMultipleChoice } from './ListeningMultipleChoice';
-export { default as ListeningFillBlank } from './ListeningFillBlank';
-export { default as ListeningMatching } from './ListeningMatching';
-export { default as ListeningTrueFalse } from './ListeningTrueFalse';
--- frontend/src/features/admin/components/reading/question-types/index.js
+++
@@ -1,4 +0,0 @@
-export { default as FillBlankAdmin } from './FillBlankAdmin';
-export { default as TrueFalseAdmin } from './TrueFalseAdmin';
-export { default as MatchingAdmin } from './MatchingAdmin';
-export { default as MultipleChoiceAdmin } from './MultipleChoiceAdmin';
