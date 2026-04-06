6884f441b12fa0eddad9c5b03b1a1400d2da0f5a
Luan_Vo <thanhluanvo25@gmail.com>
2026-04-06 15:36:49 +0700

    update main orgins

--- backend/main.py
+++ backend/main.py
@@ -76,7 +76,10 @@ app = FastAPI(
 # ==========================================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",                       
+        "https://ielts-aptis-frontend.onrender.com"    
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
