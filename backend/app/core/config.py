1ca323ee8d82ec4e58945bbeb6915ee4399012fe
Luan_Vo <thanhluanvo25@gmail.com>
2026-04-06 22:02:25 +0700

    update config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     GOOGLE_CLIENT_ID: str
 
     USE_CLOUDINARY: bool = True
+    BASE_URL: str = "http://localhost:8000"
     CLOUDINARY_CLOUD_NAME: str
     CLOUDINARY_API_KEY: str
     CLOUDINARY_API_SECRET: str
