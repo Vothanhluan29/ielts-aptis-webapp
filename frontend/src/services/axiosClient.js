826bc249110a78c35751b36aa31de37247d2cff9
Luan_Vo <thanhluanvo25@gmail.com>
2026-04-06 15:18:41 +0700

    update link URL for login local and public web

--- frontend/src/services/axiosClient.js
+++ frontend/src/services/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast'; // Import thư viện toast để thông báo đẹp hơn
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_BASE_URL || import.meta.env.VITE_API_BASE_URL_LOCAL,
   headers: {
     'Content-Type': 'application/json',
   },
