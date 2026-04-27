102ed5eeb70d5d01ead8089e3c8fe480969dc5b8
Luan_Vo <thanhluanvo25@gmail.com>
2026-04-27 13:12:22 +0700

    updatw user management api for admin

--- frontend/src/features/admin/api/users/adminUserApi.js
+++ frontend/src/features/admin/api/users/adminUserApi.js
@@ -1,25 +1,17 @@
 import axiosClient from '../../../../services/axiosClient';
 
 const adminUserApi = {
-  // 1. Lấy tất cả (Dùng mặc định skip=0, limit=100 như Backend)
- getAllUsers: (skip = 0, limit = 1000) => {
-    return axiosClient.get(`/users/?skip=${skip}&limit=${limit}`);
-  },
+  getAllUsers: (skip = 0, limit = 1000) =>
+    axiosClient.get(`/users/?skip=${skip}&limit=${limit}`),
 
-  // 2. Lấy chi tiết
-  getUserDetail: (userId) => {
-    return axiosClient.get(`/users/${userId}`);
-  },
+  getUserDetail: (userId) =>
+    axiosClient.get(`/users/${userId}`),
 
-  // 3. Cập nhật (full_name, role, is_active)
-  updateUserByAdmin: (userId, updateData) => {
-    return axiosClient.patch(`/users/${userId}`, updateData);
-  },
+  updateUserByAdmin: (userId, data) =>
+    axiosClient.patch(`/users/${userId}`, data),
 
-  // 4. Xóa tài khoản
-  deleteUserByAdmin: (userId) => {
-    return axiosClient.delete(`/users/${userId}`);
-  }
+  deleteUserByAdmin: (userId) =>
+    axiosClient.delete(`/users/${userId}`),
 };
 
 export default adminUserApi;
