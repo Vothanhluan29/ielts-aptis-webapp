2eb100cf0718e07a0a17cd5662f5382adeaaba98
Luan_Vo <thanhluanvo25@gmail.com>
2026-04-27 22:27:57 +0700

    update GV services

--- backend/app/modules/APTIS/grammar_vocab/services/utils.py
+++ backend/app/modules/APTIS/grammar_vocab/services/utils.py
@@ -3,7 +3,7 @@ import json
 class GrammarVocabUtils:
     @staticmethod
     def parse_options(options):
-        """Hàm an toàn để parse chuỗi JSON từ DB về dạng dictionary."""
+
         if isinstance(options, dict):
             return options
         if isinstance(options, str):
