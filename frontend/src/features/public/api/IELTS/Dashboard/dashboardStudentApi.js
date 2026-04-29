e5c054701c1ea6413cea7f69e3a3562bc5a8121b
Luan_Vo <thanhluanvo25@gmail.com>
2026-04-29 20:51:51 +0700

    update dashboardstudentApi

--- frontend/src/features/public/api/IELTS/Dashboard/dashboardStudentApi.js
+++ frontend/src/features/public/api/IELTS/Dashboard/dashboardStudentApi.js
@@ -1,20 +1,19 @@
-import axiosClient from '../../../../../services/axiosClient'; // Đảm bảo đường dẫn này khớp với project của bạn
+import axiosClient from '../../../../../services/axiosClient'; 
 
 const BASE_URL = '/stats';
 
 export const dashboardStudentApi = {
-  // 1. Lấy thống kê tổng quan (Điểm trung bình Full Test & 4 Kỹ năng)
+
   getOverviewStats: () => {
     return axiosClient.get(`${BASE_URL}/overview`);
   },
 
-  // 2. Lấy dữ liệu biểu đồ (Chart) và Chuỗi ngày học (Streak)
+
   getProgressData: () => {
     return axiosClient.get(`${BASE_URL}/progress`);
   },
 
-  // 3. Lấy danh sách hoạt động gần đây (Recent Activities)
-  // Truyền tham số limit để giới hạn số lượng bài hiển thị (Mặc định 10)
+
   getRecentActivities: (limit = 10) => {
     return axiosClient.get(`${BASE_URL}/activities`, {
       params: { limit }
